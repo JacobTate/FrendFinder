341a69b6b810908f3f1e8a9b69473bffc44915d5
Jacob Tate <jacob87608@gmail.com>
2020-03-17 09:36:40 -0500

    worked on the compare logic it works but needs more work

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,39 @@ var frendList = [{
         "1",
         "4"
     ]
-}];
+},
+{
+    Name: "jill",
+    Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0jQw-B9HzW29FyJXjCIpVvA72MARtC25Dofimc_cKf9NS-FSV&s",
+    scores: [
+        "4",
+        "3",
+        "1",
+        "3",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "1"
+    ]
+},
+{
+    Name: "jan",
+    Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0jQw-B9HzW29FyJXjCIpVvA72MARtC25Dofimc_cKf9NS-FSV&s",
+    scores: [
+        "5",
+        "4",
+        "2",
+        "3",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "1"
+    ]
+}
+];
 
 module.exports = frendList
